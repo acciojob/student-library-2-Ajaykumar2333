c83d5ccec014218db241ae392e9368c887a95fce
ajaya <vodlaajay234@gmail.com>
2023-01-27 14:38:35 +0530

    second commit

--- src/main/java/com/driver/models/Book.java
+++ src/main/java/com/driver/models/Book.java
@@ -95,14 +95,11 @@ public class Book {
         this.transactions = transactions;
     }
 
-    public Book(int id, String name, Genre genre, Author author, Card card, boolean available, List<Transaction> transactions) {
-        this.id = id;
+    public Book(String name, Genre genre, Author author, boolean available) {
         this.name = name;
         this.genre = genre;
         this.author = author;
-        this.card = card;
         this.available = available;
-        this.transactions = transactions;
     }
 }
 
--- src/main/java/com/driver/models/Student.java
+++ src/main/java/com/driver/models/Student.java
@@ -115,14 +115,10 @@ public class Student {
                 '}';
     }
 
-    public Student(int id, String emailId, String name, int age, String country, Card card, Date createdOn, Date updatedOn) {
-        this.id = id;
+    public Student(String emailId, String name, int age, String country) {
         this.emailId = emailId;
         this.name = name;
         this.age = age;
         this.country = country;
-        this.card = card;
-        this.createdOn = createdOn;
-        this.updatedOn = updatedOn;
     }
 }
