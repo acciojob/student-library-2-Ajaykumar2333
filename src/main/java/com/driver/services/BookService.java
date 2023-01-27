da8d09f8d92c0dba7d11b8629c42a925cdee6319
ajaya <vodlaajay234@gmail.com>
2023-01-27 17:08:03 +0530

    message11

--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -22,8 +22,8 @@ public class BookService {
 
     public void createBook(Book book){
         try {
-            int authorId = book.getAuthor().getId();
-            Author author = authorRepository.findById(authorId).get();
+            int id = book.getAuthor().getId();
+            Author author = authorRepository.findById(id).get();
             List<Book> bookList = author.getBooksWritten();
             if(bookList==null) {
                 bookList = new ArrayList<>();
@@ -39,20 +39,21 @@ public class BookService {
             bookRepository2.save(book);
         }
     }
+    public List<Book> getBooks(String genre, boolean available, String author){
+        List<Book> books;
 
-    public List<Book> getBooks(String genre, boolean available, String author) {
-//
-        if (genre != null && author != null) {
-            return bookRepository2.findBooksByGenreAuthor(genre, author, available);
-        } else if (genre != null) {
-            return bookRepository2.findBooksByGenre(genre, available);
-        } else if (author != null) {
-            return bookRepository2.findBooksByAuthor(author, available);
-
-        } else {
-            return bookRepository2.findByAvailability(available);
+        if(genre != null && author != null){
+            books = bookRepository2.findBooksByGenreAuthor(genre, author, available);
+        }else if(genre != null){
+            books = bookRepository2.findBooksByGenre(genre, available);
+        }else if(author != null){
+            books = bookRepository2.findBooksByAuthor(author, available);
+        }else{
+            books = bookRepository2.findByAvailability(available);
         }
+        return books;
     }
+
 
 
 
